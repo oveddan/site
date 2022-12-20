246845894f1ca88fc9836ad946d7ea9f21143979
Dan Oved <stangogh@gmail.com>
2022-12-19 17:27:28 -0800

    formatted tags

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -18,8 +18,12 @@ const FilterChipComponent = ({
   return (
     <button
       className={clsx(
-        'inline-flex items-center px-3 text-sm font-medium m-1 py-1 rounded-lg border border-solid border-black',
-        { 'bg-orange-500': selected, 'bg-gray-200 text-gray-500': notSelected }
+        'inline-flex items-center px-3 text-sm font-medium m-1 py-1 rounded-lg border border-solid border-black dark:text-white dark:border-white',
+        {
+          'bg-orange-500': selected,
+          'bg-gray-200 text-gray-500 border-gray-300 hover:border-black hover:text-black dark:bg-gray-700 dark:border-gray-400 dark:text-gray-400 dark:hover:text-white dark:hover:border-white':
+            notSelected,
+        }
       )}
       onClick={() => toggle(chip.filterType, chip.value)}
     >
@@ -49,19 +53,21 @@ const Filters = ({ filters, activeFilters, toggleFilter }: FilterProps) => {
   const anySelected = filterKeys.length > 0;
 
   return (
-    <div className="my-4">
-      {filterChips.map((chip) => {
-        const key = `${chip.filterType}-${chip.value}`;
-        return (
-          <FilterChipComponent
-            key={key}
-            chip={chip}
-            toggle={toggleFilter}
-            anySelected={anySelected}
-            selected={!!activeFilters[chip.filterType]?.has(chip.value)}
-          />
-        );
-      })}
+    <div className="hidden md:block">
+      <div className="my-16">
+        {filterChips.map((chip) => {
+          const key = `${chip.filterType}-${chip.value}`;
+          return (
+            <FilterChipComponent
+              key={key}
+              chip={chip}
+              toggle={toggleFilter}
+              anySelected={anySelected}
+              selected={!!activeFilters[chip.filterType]?.has(chip.value)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
