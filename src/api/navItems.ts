14f1d8210c2e5e96b87ab6cad02313d349b427aa
Dan Oved <stangogh@gmail.com>
2022-12-21 22:08:33 -0800

    hide about until its ready

--- src/api/navItems.ts
+++ src/api/navItems.ts
@@ -3,10 +3,10 @@ export const navItems: { text: string; path: string }[] = [
     text: 'portfolio',
     path: '/',
   },
-  {
-    text: 'about',
-    path: '/about',
-  },
+  // {
+  //   text: 'about',
+  //   path: '/about',
+  // },
   // {
   //   text: 'blog',
   //   path: '/blog',
