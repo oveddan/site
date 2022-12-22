ae0cd4767af8796e83897f11a542bd9755006ce7
Dan Oved <stangogh@gmail.com>
2022-12-21 17:54:24 -0800

    added visual massage

--- src/api/types.ts
+++ src/api/types.ts
@@ -17,6 +17,7 @@ export enum Category {
 
 export enum Tech {
   React = 'React',
+  Solidity = 'Solidity',
   Javascript = 'Javascript',
   Tensorflow = 'Tensorflow',
   Tensorflowjs = 'TensorflowJs',
